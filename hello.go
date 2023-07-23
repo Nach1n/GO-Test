d34317ff1036693b20c2ff391e89f1c8ce790a37
Nach1n <l.ancavilpino@gmail.com>
2023-07-23 14:07:44 -0400

    chore(*): Added new changes to print some variables and other stuffs

--- hello.go
+++ hello.go
@@ -3,6 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
-}
+	var conferenceName = "Go Conference"
+	const conferenceTickets = 50
+	var remainingTickets = 50
 
+	fmt.Printf("Welcome to %v booking app\n", conferenceName)
+	fmt.Printf("We have total of %v tickets and %v are still available.", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend")
+
+	var userName string
+	var userTickets int
+	// ask user for his name
+	userName = "Tom"
+	userTickets = 2
+
+	fmt.Printf("User %v booked %v tickets \n", userName, userTickets)
+
+}
