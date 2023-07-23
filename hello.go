f649be585ad37ebee7e4176cbb60f9762991f416
Nach1n <l.ancavilpino@gmail.com>
2023-07-23 15:08:18 -0400

    feat(*): Added new logic to scan input from users and also, added logic to calculate remainingTickets

--- hello.go
+++ hello.go
@@ -3,20 +3,37 @@ package main
 import "fmt"
 
 func main() {
-	var conferenceName = "Go Conference"
-	const conferenceTickets = 50
-	var remainingTickets = 50
+	conferenceName := "Go Conference"
+	const conferenceTickets int = 50
+	var remainingTickets uint = 50
+
+	fmt.Printf("conferenceTickets is %d, remainingTickets is %d, conferenceName is %s\n", conferenceTickets, remainingTickets, conferenceName)
 
 	fmt.Printf("Welcome to %v booking app\n", conferenceName)
 	fmt.Printf("We have total of %v tickets and %v are still available.", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	var userName string
-	var userTickets int
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
 	// ask user for his name
-	userName = "Tom"
-	userTickets = 2
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&firstName)
 
-	fmt.Printf("User %v booked %v tickets \n", userName, userTickets)
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
 
+	fmt.Println("Enter your email: ")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter number of tickets: ")
+	fmt.Scan(&userTickets)
+
+	remainingTickets = remainingTickets - userTickets
+
+	// if remainingTickets <= 0 -> error or disallow create new orders on the system
+
+	fmt.Printf("Thanks you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
 }
